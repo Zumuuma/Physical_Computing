84db3d9113ef0de8948488b6dc3baccf385a7289
かとぽよ☆ <69085860+shinexkazuma@users.noreply.github.com>
2020-10-25 16:56:23 +0900

    LEDを使って0から7までの数を表現するVol2

--- C/flow_from_0_to_7.c
+++ C/flow_from_0_to_7.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <wiringPi.h>
 
-#define pin_1 0 //０番ポート使用。ラズパイ１１番ピンに接続。
-#define pin_2 3 //３番ポート使用。ラズパイ１５番ピンに接続。
-#define pin_3 7 //７番ポート使用。ラズパイ７番ピンに接続。
+#define PIN_1 0 //０番ポート使用。ラズパイ１１番ピンに接続。
+#define PIN_2 3 //３番ポート使用。ラズパイ１５番ピンに接続。
+#define PIN_3 7 //７番ポート使用。ラズパイ７番ピンに接続。
 
 int data[3] = {0}; //ここに保存。０で初期化
 int x = 0;		   //変換したい１０進数
@@ -29,9 +29,9 @@ int convert_from_10_to_2(void) //１０進数から２進数に変換する関�
 int main(void)
 {
 	wiringPiSetup();		//https://projects.drogon.net/raspberry-pi/wiringpi/pins/を参照してポート番号を照らし合わせる。
-	pinMode(pin_1, OUTPUT); //出力モード
-	pinMode(pin_2, OUTPUT); //出力モード
-	pinMode(pin_3, OUTPUT); //出力モード
+	pinMode(PIN_1, OUTPUT); //出力モード
+	pinMode(PIN_2, OUTPUT); //出力モード
+	pinMode(PIN_3, OUTPUT); //出力モード
 
 	for (;;)
 	{
@@ -39,29 +39,29 @@ int main(void)
 
 		if (data[2] == 1)
 		{
-			digitalWrite(pin_1, HIGH); //+３.３Vをオン
+			digitalWrite(PIN_1, HIGH); //+３.３Vをオン
 		}
 		else
 		{
-			digitalWrite(pin_1, LOW); //+３.３Vをオフ。つまり、０V
+			digitalWrite(PIN_1, LOW); //+３.３Vをオフ。つまり、０V
 		}
 
 		if (data[1] == 1)
 		{
-			digitalWrite(pin_2, HIGH); //+３.３Vをオン
+			digitalWrite(PIN_2, HIGH); //+３.３Vをオン
 		}
 		else
 		{
-			digitalWrite(pin_2, LOW); //+３.３Vをオフ。つまり、０V
+			digitalWrite(PIN_2, LOW); //+３.３Vをオフ。つまり、０V
 		}
 
 		if (data[0] == 1)
 		{
-			digitalWrite(pin_3, HIGH); //+３.３Vをオン
+			digitalWrite(PIN_3, HIGH); //+３.３Vをオン
 		}
 		else
 		{
-			digitalWrite(pin_3, LOW); //+３.３Vをオフ。つまり、０V
+			digitalWrite(PIN_3, LOW); //+３.３Vをオフ。つまり、０V
 		}
 
 		delay(500); //xの値をインクリメントするまでの時間(ミリ秒)
