d27babffe477bd7093f14c4a115315e95003d332
かとぽよ☆ <69085860+shinexkazuma@users.noreply.github.com>
2020-10-29 16:21:41 +0900

    PWMのテスト。100Hzを使用。

---
+++ C/PWM_test.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <wiringPi.h>
+
+#define PWM_PIN 1 //‚P”Ôƒ|[ƒgg—pBƒ‰ƒYƒpƒC‚P‚Q”Ôƒsƒ“‚ÉÚ‘±B
+
+void loop()
+{
+	for (;;)
+	{
+		pwmWrite(PWM_PIN, 1600); //pwmWrite(pin”Ô†,ƒfƒ…[ƒeƒB[”ä(‚O`pwm range‚Ì”ÍˆÍ)) (—áF‚W‚O“(‚P‚U‚O‚O/‚Q‚O‚O‚O))
+		delay(500);
+		pwmWrite(PWM_PIN, 200); //pwmWrite(pin”Ô†,ƒfƒ…[ƒeƒB[”ä(‚O`pwm range‚Ì”ÍˆÍ)) (—áF‚P‚O“(‚Q‚O‚O/‚Q‚O‚O‚O))
+		delay(500);
+	}
+}
+
+int main(void)
+{
+	wiringPiSetup(); //https://projects.drogon.net/raspberry-pi/wiringpi/pins/‚ğQÆ‚µ‚Äƒ|[ƒg”Ô†‚ğÆ‚ç‚µ‡‚í‚¹‚éB
+	pinMode(PWM_PIN, PWM_OUTPUT);
+	pwmSetMode(PWM_MODE_MS);
+	//PWMü”g”  ‚P‚X.‚QMHz / (CLOCK’l – RANGE’l)@(—áF‚P‚X,‚Q‚O‚O,‚O‚O‚O / (‚Q,‚O‚O‚O * ‚X‚U) = ‚P‚O‚O (Hz))
+	pwmSetClock(96);   //CLOCK’l
+	pwmSetRange(2000); //RANGE’l
+
+	loop();
+
+	return 0;
+}
