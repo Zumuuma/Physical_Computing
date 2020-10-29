e5172375e36d22722a372fd71d299624887badb7
かとぽよ☆ <69085860+shinexkazuma@users.noreply.github.com>
2020-10-29 16:19:21 +0900

    LEDを使って0から7までの数を表現するプログラム。

--- C/expression_with_LEDs.c
+++ C/expression_with_LEDs.c
@@ -6,23 +6,28 @@
 #define PIN_2 3 //３番ポート使用。ラズパイ１５番ピンに接続。
 #define PIN_3 7 //７番ポート使用。ラズパイ７番ピンに接続。
 
-int data[3] = {0}; //２進数を保存。０で初期化
+int leds[3] = {PIN_1, PIN_2, PIN_3};
 
-int convert_from_10_to_20(void) //１０進数から２進数に変換する関数
+void convert_from_10_to_2(int num) //１０進数から２進数に変換してLEDを光らせる関数
 {
-	int x = 5;	 //変換したい１０進数
-	int tmp = 0; //余りを代入する変数。０で初期化
-
-	for (int i = 0; i < 3; i++)
+	for (int i = 2; i >= 0; i--)
 	{
-		tmp = x % 2;   //余りを求めてtmpに代入
-		x = x / 2;	   //変換したい１０進数を２で割る
-		data[i] = tmp; //２進数を保存するところに余り(tmp)を代入
+		if (num % 2 == 1)
+		{
+			digitalWrite(leds[i], HIGH);
+		}
+		num = num / 2; //変換したい１０進数を２で割る
 	}
+}
 
-	printf("%d %d %d\n", data[2], data[1], data[0]); //コマンドプロンプト上で値を表示
+void loop()
+{
+	int num = 6;
 
-	return 0;
+	for (;;)
+	{
+		convert_from_10_to_2(num);
+	}
 }
 
 int main(void)
@@ -32,34 +37,7 @@ int main(void)
 	pinMode(PIN_2, OUTPUT); //出力モード
 	pinMode(PIN_3, OUTPUT); //出力モード
 
-	convert_from_10_to_20();
-
-	if (data[2] == 1)
-	{
-		digitalWrite(PIN_1, HIGH); //+３.３Vをオン
-	}
-	else
-	{
-		digitalWrite(PIN_1, LOW); //+３.３Vをオフ。つまり、０V
-	}
-
-	if (data[1] == 1)
-	{
-		digitalWrite(PIN_2, HIGH); //+３.３Vをオン
-	}
-	else
-	{
-		digitalWrite(PIN_2, LOW); //+３.３Vをオフ。つまり、０V
-	}
-
-	if (data[0] == 1)
-	{
-		digitalWrite(PIN_3, HIGH); //+３.３Vをオン
-	}
-	else
-	{
-		digitalWrite(PIN_3, LOW); //+３.３Vをオフ。つまり、０V
-	}
+	loop();
 
 	return 0;
 }
