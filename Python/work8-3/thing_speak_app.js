c34a869542a422e75c29ed80cc357452be35f73c
かとぽよ☆ <69085860+shinexkazuma@users.noreply.github.com>
2020-11-11 14:23:40 +0900

    実験3. ThingSpeakからのデータをWebページに表示する

---
+++ Python/work8-3/thing_speak_app.js
@@ -0,0 +1,46 @@
+window.onload = Main;
+
+const base_url = "https://api.thingspeak.com/channels/1225088/feeds.json";
+const api_key = "0PNGGPVT4PU6H5K8"
+
+let app;
+
+function Main() {
+	app = new Vue({
+		el: "#app",
+		data: {
+			createdAt: [],
+			brightness: []
+		},
+		mounted: function (event) {
+			updateData()
+			setInterval(updateData, 15000);
+		}
+	});
+}
+
+
+function updateData() {
+	const result_num = 5
+	let url = base_url +
+		"?api_key=" +
+		api_key +
+		"&timezone=Asia/Tokyo" +
+		"&results=" +
+		result_num
+	fetch(url, { method: 'GET' })
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (res) {
+			// console.log(res.feeds);
+			let createdAt = []
+			let brightness = []
+			res.feeds.forEach(elm => {
+				createdAt.push(elm.created_at);
+				brightness.push(elm.field1);
+			});
+			app.createdAt = createdAt;
+			app.brightness = brightness;
+		});
+}
